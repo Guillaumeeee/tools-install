a7df36a1cf00f1d64d50a2ec8d5025981b1202b5
tiix <35033482+tiix95@users.noreply.github.com>
2019-04-16 14:42:44 +0200

    Update squid.sh

--- squid.sh
+++ squid.sh
@@ -1,47 +1,50 @@
 #! /bin/bash
 
-# Installer les dépendances
+# Install dependancies
 apt-get update
 apt-get -y install libssl1.0-dev
 
-# Télécharger la dernière version de squid
+# Download Squid 4.6 source code
 cd /opt && wget http://www.squid-cache.org/Versions/v4/squid-4.6.tar.gz
 
-# Décompresser l'archive
+# Untar archive
 tar xvzf squid-4.6.tar.gz
 
-# Configurer le fichier de compilation
+# Configure the compiler with ssl options
 cd squid-4.6
 ./configure --with-openssl --enable-ssl --enable-ssl-crtd --disable-ipv6
 
-# Compiler le code source
+# Compile
 make
 
-# Installer l'outil
+# Install
 make install
 
-# Créer un utilisateur squid pour l'appartenance des fichiers (cache, certificats, logs...)
+# Create squid user for file permissions (cache, certificates, logs...)
 groupadd squid
 useradd squid -r -s /sbin/nologin -g squid
 
-# Créer le répertoire où placer les certificats
+# Create certificates directory
 mkdir /usr/local/squid/ssl
 cd /usr/local/squid/ssl
 
-# Créer la clé privée et le certificat de la CA
-openssl req -new -newkey rsa:4096 -sha256 -days 3650 -nodes -x509 -extensions v3_ca -keyout proxyCA.pem -out proxyCA.pem
+# Create CA certificate and private key
+openssl req -new -newkey rsa:4096 -sha256 -days 3650 -nodes -x509 -extensions v3_ca -keyout proxyCA.pem -out proxyCA.pem -subj "/C=FR/ST=IDF/L=Paris/O=TIIX/OU=WTF/CN=proxy.tiix.lab"
 
-# Générer le certificat au format der (import dans un navigateur)
+# Export certificate to "der" format (importable into browser)
 openssl x509 -in proxyCA.pem -outform DER -out proxyCA.der
 
-# Configurer les permissions
+# Configure permissions
 chown -R squid:squid /usr/local/squid/ssl
 
-# Créer les répertoire de cache et de log
+# Create cache & logs directories
 mkdir -p /var/squid/cache
 mkdir -p /var/log/squid
 chown -R squid:squid /var/squid/cache
 chown -R squid:squid /var/log/squid
 
-# Initialiser la base de certificats 
+# Initialyze ssl cert database
 /usr/local/squid/libexec/security_file_certgen -c -s /usr/local/squid/var/cache/squid/ssl_db -M 4MB
+
+# Initialyze cache directory
+/usr/local/squid/sbin/squid -z
