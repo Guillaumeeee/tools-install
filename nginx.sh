dd46e002c251d59996a495c60cc9cbff4c3cb476
tiix <35033482+tiix95@users.noreply.github.com>
2019-05-03 14:36:54 +0200

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -7,5 +7,9 @@ mkdir /etc/nginx/ssl
 chown 700 /etc/nginx/ssl
 cd /etc/nginx/ssl
 
-openssl req -new -newkey rsa:4096 -sha256 -days 3650 -nodes -x509 -extensions v3_ca -keyout webCA.key -out webCA.crt -subj "/C=FR/ST=IDF/L=Paris/O=TIIX/OU=WTF/CN=web.tiix.lab"
+openssl req -new -newkey rsa:4096 -sha256 -days 3650 -nodes -x509 -extensions v3_ca -keyout webServ.key -out webServ.crt -subj "/C=FR/ST=IDF/L=Paris/O=TIIX/OU=WTF/CN=web.tiix.lab"
+openssl dhparam 2048 > dh2048.pem
 
+cd -
+cp config/nginx.conf /etc/nginx/sites-available/webServ.cfg
+ln -s /etc/nginx/sites-available/webServ.cfg /etc/nginx/sites-enabled/webServ.cfg
