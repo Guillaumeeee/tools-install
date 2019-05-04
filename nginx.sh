3e0eb63741a13cf5ca54c6c2e87c1b6d137a9425
tiix <35033482+tiix95@users.noreply.github.com>
2019-05-04 10:14:27 +0200

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -10,7 +10,7 @@ echo "Welcome on our default nginx site"  > /var/www/html/index.html
 mkdir /etc/nginx/ssl
 
 # Change rights on SSL directory
-chown 700 /etc/nginx/ssl
+chmod 700 /etc/nginx/ssl
 
 # Create Diffie-Helmann parameters
 openssl dhparam 2048 > /etc/nginx/ssl/dh2048.pem
@@ -18,11 +18,11 @@ openssl dhparam 2048 > /etc/nginx/ssl/dh2048.pem
 # Create CA certificate and key
 # openssl req -new -newkey rsa:4096 -sha256 -days 3650 -nodes -x509 -extensions v3_ca -keyout /etc/nginx/ssl/webCA.key -out /etc/nginx/ssl/webCA.crt -subj "/C=FR/ST=IDF/L=Paris/O=TIIX/OU=WTF/CN=webCA.tiix.lab"
 openssl genrsa -out /etc/nginx/ssl/webCA.key 2048
-openssl req -x509 -new -nodes -key /etc/nginx/ssl/webCA.key -sha256 -days 3650 -out /etc/nginx/ssl/webCA.pem
+openssl req -x509 -new -nodes -key /etc/nginx/ssl/webCA.key -sha256 -days 3650 -out /etc/nginx/ssl/webCA.pem -subj "/C=FR/ST=IDF/L=Paris/O=TIIX/OU=WTF/CN=webCA.tiix.lab"
 
 # Create server certificate and key
 openssl genrsa -out /etc/nginx/ssl/webServ.key 2048
-openssl req -new -key /etc/nginx/ssl/webServ.key -out /etc/nginx/ssl/webServ.csr -subj "/C=FR/ST=IDF/L=Paris/O=TIIX/OU=WTF/CN=webCA.tiix.lab"
+openssl req -new -key /etc/nginx/ssl/webServ.key -out /etc/nginx/ssl/webServ.csr -subj "/C=FR/ST=IDF/L=Paris/O=TIIX/OU=WTF/CN=web.tiix.lab"
 openssl x509 -req -in /etc/nginx/ssl/webServ.csr -CA /etc/nginx/ssl/webCA.pem -CAkey /etc/nginx/ssl/webCA.key -CAcreateserial -out /etc/nginx/ssl/webServ.crt -days 365 -sha256 
 
 # Copy nginx site config to nginx directory
