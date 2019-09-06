b20100f081479cb98f2fa84c5253dd0eaebcabb3
tiix95 <tiix95@github.com>
2019-09-06 11:04:34 +0200

    squid dependancy add

--- squid.sh
+++ squid.sh
@@ -3,7 +3,7 @@
 
 # Install dependancies
 apt-get update
-apt-get -y install libssl1.0-dev gcc make 
+apt-get -y install libssl1.0-dev gcc make g++
 
 # Download Squid 4.6 source code
 cd /opt && wget http://www.squid-cache.org/Versions/v4/squid-4.6.tar.gz
