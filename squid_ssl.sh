3b8b57bd767fe46de0111fc2981a9db5e4958e69
tiix <35033482+tiix95@users.noreply.github.com>
2019-12-17 11:50:12 +0100

    upload squid_ssl files

--- squid_ssl.sh
+++ squid_ssl.sh
@@ -13,7 +13,7 @@ tar xvzf squid-4.6.tar.gz
 
 # Configure the compiler with ssl options
 cd squid-4.6
-./configure --disable-ipv6
+./configure --with-openssl --enable-ssl --enable-ssl-crtd --disable-ipv6
 
 # Compile
 make
@@ -25,11 +25,27 @@ make install
 groupadd squid
 useradd squid -r -s /sbin/nologin -g squid
 
+# Create certificates directory
+mkdir /usr/local/squid/ssl
+cd /usr/local/squid/ssl
+
+# Create CA certificate and private key
+openssl req -new -newkey rsa:4096 -sha256 -days 3650 -nodes -x509 -extensions v3_ca -keyout proxyCA.pem -out proxyCA.pem -subj "/C=FR/ST=IDF/L=Paris/O=TIIX/OU=WTF/CN=proxy.tiix.lab"
+
+# Export certificate to "der" format (importable into browser)
+openssl x509 -in proxyCA.pem -outform DER -out proxyCA.der
+
+# Configure permissions
+chown -R squid:squid /usr/local/squid/ssl
+
 # Create cache & logs directories
 mkdir -p /var/squid/cache
 mkdir -p /var/log/squid
 chown -R squid:squid /var/squid/cache
 chown -R squid:squid /var/log/squid
+
+# Initialyze ssl cert database
+/usr/local/squid/libexec/security_file_certgen -c -s /usr/local/squid/var/cache/squid/ssl_db -M 4MB
 
 # Initialyze cache directory
 /usr/local/squid/sbin/squid -z
