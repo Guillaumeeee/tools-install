dd96e305bcdf92f551a117cad18db72d0fa65a9c
tiix <35033482+tiix95@users.noreply.github.com>
2019-12-18 15:55:38 +0100

    Update squid.sh

--- squid.sh
+++ squid.sh
@@ -12,31 +12,32 @@ wget http://www.squid-cache.org/Versions/v4/squid-4.6.tar.gz -O /opt/squid-4.6.t
 wget http://www.squidguard.org/Downloads/squidGuard-1.3.tar.gz -O /opt/squidGuard-1.3.tar.gz
 
 # Télécharger les blacklists de squidguard
-wget ftp://ftp.univ-tlse1.fr/pub/reseau/cache/squidguard_contrib/blacklists.tar.gz -O /opt/blacklists.tar.gz
+wget http://squidguard.mesd.k12.or.us/blacklists.tgz -O /opt/blacklists.tar.gz
+
+# Download berkeley db needed for squidguard
+wget https://www.tiix.wtf/public_share/berkeley_db_18.1.32.tar.gz -O /opt/berkeley_db_18.1.32.tar.gz
 
 # Untar archive
 tar xzf /opt/squid-4.6.tar.gz -C /opt
 tar xzf /opt/squidGuard-1.3.tar.gz -C /opt
-tar xzf blacklists.tar.gz -C /var/lib/squidguard/db/
+tar xzf /opt/blacklists.tar.gz -C /opt
+tar xzf /opt/berkeley_db_18.1.32.tar.gz -C /opt
 
 # Configure the compiler with ssl options
 cd /opt/squid-4.6
 ./configure --disable-ipv6
-
-# Compile
 make
-
-# Install
 make install
 
-# Configure the compiler with ssl options
-cd /opt/squidGuard-1.3.tar.gz
-./configure
-
-# Compile
+cd /opt/db-18.1.32/build_unix
+../dist/configure --prefix=/usr/local/berkeleydb
 make
+make install 
 
-# Install
+# Configure the compiler with ssl options
+cd /opt/squidGuard-1.3 
+./configure --with-db=/usr/local/berkeleydb
+make
 make install
 
 # Create squidguard configuration
