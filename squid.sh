6672e5edb37b268b36933866368b3bb29b604568
tiix <35033482+tiix95@users.noreply.github.com>
2019-12-18 12:26:42 +0100

    Update squid.sh

--- squid.sh
+++ squid.sh
@@ -6,7 +6,7 @@ apt-get update
 apt-get -y install libssl1.0-dev gcc make g++
 
 # Download Squid 4.6 source code
-cd /opt && wget http://www.squid-cache.org/Versions/v4/squid-4.6.tar.gz
+wget http://www.squid-cache.org/Versions/v4/squid-4.6.tar.gz -O /opt/squid-4.6.tar.gz
 
 # Untar archive
 tar xvzf squid-4.6.tar.gz
@@ -30,6 +30,9 @@ mkdir -p /var/squid/cache
 mkdir -p /var/log/squid
 chown -R squid:squid /var/squid/cache
 chown -R squid:squid /var/log/squid
+
+# Télécharger les blacklists de squidguard
+wget ftp://ftp.univ-tlse1.fr/pub/reseau/cache/squidguard_contrib/blacklists.tar.gz -O /opt/blacklists.tar.gz
 
 # Initialyze cache directory
 /usr/local/squid/sbin/squid -z
