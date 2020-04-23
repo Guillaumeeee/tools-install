8978d6d1e10817ca3595b2ef33e09b913744fb20
tiix <contact@tiix.wtf>
2020-04-23 10:09:36 -0400

    squid update

--- squid.sh
+++ squid.sh
@@ -6,7 +6,7 @@ apt-get update
 apt-get -y install gcc make g++ libpcre3-dev zlib1g-dev libluajit-5.1-dev libpcap-dev openssl libnghttp2-dev libdumbnet-dev bison flex libdnet libc-bin libssl1.1 libssl-dev
 
 # Download Squid 4.6 source code
-wget http://www.squid-cache.org/Versions/v4/squid-4.6.tar.gz -O /opt/squid-4.6.tar.gz
+wget http://www.squid-cache.org/Versions/v4/squid-4.11.tar.gz -O /opt/squid-4.11.tar.gz
 
 # Download squidguard
 wget http://www.squidguard.org/Downloads/squidGuard-1.3.tar.gz -O /opt/squidGuard-1.3.tar.gz
@@ -18,13 +18,13 @@ wget http://squidguard.mesd.k12.or.us/blacklists.tgz -O /opt/blacklists.tar.gz
 wget https://www.tiix.wtf/public_share/berkeley_db_18.1.32.tar.gz -O /opt/berkeley_db_18.1.32.tar.gz
 
 # Untar archive
-tar xzf /opt/squid-4.6.tar.gz -C /opt
+tar xzf /opt/squid-4.11.tar.gz -C /opt
 tar xzf /opt/squidGuard-1.3.tar.gz -C /opt
 tar xzf /opt/blacklists.tar.gz -C /opt
 tar xzf /opt/berkeley_db_18.1.32.tar.gz -C /opt
 
 # Configure the compiler with ssl options
-cd /opt/squid-4.6
+cd /opt/squid-4.11
 ./configure --disable-ipv6
 make
 make install
@@ -43,8 +43,12 @@ make
 make install
 
 # Create squidguard configuration
-#cp config/squidGuard.conf /usr/local/squigduard
-#ln -s /usr/local/squidguard/squidGuard.conf /usr/local/squid
+cp config/squidGuard.conf /usr/local/squigduard
+ln -s /usr/local/squidguard/squidGuard.conf /usr/local/squid
+
+# Copy config
+cp /usr/local/squid/etc/squid.conf /usr/local/squid/etc/squid.conf.dist
+cp config/squid.conf /usr/local/squid/etc/
 
 # Create squid user for file permissions (cache, certificates, logs...)
 /usr/sbin/groupadd squid
